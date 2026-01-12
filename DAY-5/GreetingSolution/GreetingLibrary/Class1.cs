7643c625aae8bcf0488da7d7a8eb89f1a0855c90
Nishant Kumar <nishantkumar260203@gmail.com>
2026-01-12 12:31:56 +0530

    Added GreetingSolution project

---
+++ DAY-5/GreetingSolution/GreetingLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return "Hello, Guest!";
+
+            return $"Hello, {name}!";
+        }
+    }
+}
