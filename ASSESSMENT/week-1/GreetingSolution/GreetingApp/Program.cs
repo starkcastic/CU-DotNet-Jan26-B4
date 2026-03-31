4dcd04b23edc0238ca778e3535b6dd38d3dd66cb
Nishant Kumar <nishantkumar260203@gmail.com>
2026-03-31 14:41:58 +0530

    week-1

---
+++ ASSESSMENT/week-1/GreetingSolution/GreetingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GreetingLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name:");
+        string? name = Console.ReadLine();
+
+        string greeting = GreetingHelper.GetGreeting(name ?? "");
+        Console.WriteLine(greeting);
+    }
+}
